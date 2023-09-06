8b808bd6eb45764f9a6173c37e3bc923ffbe2b50
Meteorych <super.titlov@inbox.ru>
2023-09-06 14:34:00 +0300

    Initial commit

---
+++ Lab1/Password.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1AOIS
+{
+    internal class Password
+    {
+        private string _password;
+        private const string valid = "абвгдеёжзийклмнпрстоуфхцчщшъыьэюя0123456789";
+        public Password(int length)
+        {
+            Random random = new Random();
+            StringBuilder buildPassword = new StringBuilder();
+            while (0 < length--)
+            {
+                buildPassword.Append(valid[random.Next(valid.Length)]);
+            }
+            _password = buildPassword.ToString();
+            Console.WriteLine(_password);
+        }
+    }
+
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiveCharts;
+
+using LiveCharts.Wpf;
+
+namespace Lab1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            CartesianChart cartesianChart1 = new CartesianChart();
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "Frequencies",
+                    Values = new ChartValues<int> { 10, 15, 7, 22, 18 }, // Replace with your frequency data
+                }
+            };
+        }
+    }
+}
