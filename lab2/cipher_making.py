40505e096e90f423bb256058e90fe801fdaa624d
Meteorych <super.titlov@inbox.ru>
2023-09-13 21:28:49 +0300

    lab2progress

---
+++ lab2/cipher_making.py
@@ -0,0 +1,43 @@
+import itertools
+import random
+import string
+import time
+from itertools import cycle
+
+alphabet = list(string.ascii_lowercase)
+
+
+def generate_keyword():
+    keyword = ""
+    for i in random.choices(alphabet, k=random.randint(5, 12)):
+        keyword += i
+    print(f"Keyword: {keyword}")
+    return keyword
+
+
+def making_cipher(keyword: str):
+    begin_word = input("Input word, that you want to cipher: ")
+    f = lambda arg: alphabet[(alphabet.index(arg[0]) + alphabet.index(arg[1]) % 26) % 26]
+    cipher_word = ''.join(map(f, zip(begin_word, cycle(keyword))))
+    print(f"\nCipher word: {cipher_word}")
+    decode_cipher(cipher_word, begin_word)
+
+
+def decode_cipher(cipher_word: str, begin_word: str):
+    start = time.time()
+    decoded_word = ""
+    for length in range(5, 12):
+        for keyword in itertools.product(alphabet, repeat=length):
+            f = lambda arg: alphabet[alphabet.index(arg[0]) - alphabet.index(arg[1]) % 26]
+            decoded_word = ''.join(map(f, zip(cipher_word, cycle(keyword))))
+            if decoded_word == begin_word:
+                break
+        if decoded_word != begin_word:
+            continue
+        else:
+            break
+    print(f"\nDecoded word: {decoded_word}")
+    end = time.time()
+    print(end - start)
+
+
--- lab2/main.py
+++ lab2/main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import cipher_making
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    keyword = cipher_making.generate_keyword()
+    begin_word = cipher_making.making_cipher(keyword)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
